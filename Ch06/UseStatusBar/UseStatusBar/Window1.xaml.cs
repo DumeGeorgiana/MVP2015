853b72dcea086f0fd82ae5b25727a456b032800b
Horea Oros <horea.oros@gmail.com>
2015-04-27 11:04:47 +0300

    add examples

---
+++ Ch06/UseStatusBar/UseStatusBar/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UseStatusBar
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+        System.Windows.Threading.DispatcherTimer dt = new System.Windows.Threading.DispatcherTimer();
+		public Window1()
+		{
+			this.InitializeComponent();
+            dt.Interval = new TimeSpan(0,0,1);
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Start();
+			// Insert code required on object creation below this point.
+            lblDate.Content = DateTime.Now.ToShortDateString();
+            lblClock.Content = DateTime.Now.ToLongTimeString();
+		}
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            lblClock.Content = DateTime.Now.ToLongTimeString();
+        }
+	}
+}
---
+++ Ch22/FrameApp/FrameApp/pagMain.xaml.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FrameApp
+{
+	public partial class pagMain
+	{
+		public pagMain()
+		{
+			this.InitializeComponent();
+
+			// Insert code required on object creation below this point.
+		}
+
+       
+
+        // Go to the AddCustomer page.
+        private void btnAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new pagAddCustomer());
+        }
+
+        // Go to the FindCustomer page.
+        private void btnFindCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new pagFindCustomer());
+        }
+
+        // Go to the Invoices page.
+        private void btnInvoices_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new pagInvoices());
+        }
+
+        // Go to the Shipping page.
+        private void btnShipping_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new pagShipping());
+        }
+
+        // Go to the help Web site.
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("http://www.vb-helper.com/wpf.htm"));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            NavigationService.NavigationFailed +=new System.Windows.Navigation.NavigationFailedEventHandler(NavigationService_NavigationFailed);
+        }
+        void NavigationService_NavigationFailed(object sender, System.Windows.Navigation.NavigationFailedEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.Message);
+        }
+	}
+}
